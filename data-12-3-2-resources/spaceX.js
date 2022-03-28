782f53e0cf496e264896b018c183b06563fb5ec0
tkiruthika <tkiruthika86@gmail.com>
2022-03-27 21:10:03 -0500

    Adding practice files

---
+++ data-12-3-2-resources/spaceX.js
@@ -0,0 +1,11 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+
+//d3.json(url).then(spaceXResults => console.log(spaceXResults[0].full_name));
+// d3.json("samples.json").then(function(data){
+//     console.log(data);
+// });
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
